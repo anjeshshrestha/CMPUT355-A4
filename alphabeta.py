f65d9469593e9e8a572a7a2286a00f0356277de4
Shawna Dawson <sndawson@ualberta.ca>
2020-11-09 21:43:31 -0700

    Implemented player and attempted heuristic
    There's an issue somewhere, likely related to the way I'm "undoing" moves.
    Seems to be when it finds a winning move through alphabeta, it doesn't undo the
    end condition state. Probably not worth looking into too much since I'm just
    using this library temporarily.
    
    Also, probably need to modify my heuristic since it always ends up negative at
    the moment. Might still work though.

--- alphabeta.py
+++ alphabeta.py
@@ -1,5 +1,5 @@
 INFINITY = 1000000
-MAX_DEPTH = 50 # Don't know how big I can make this
+MAX_DEPTH = 10 # Don't know how big I can make this
 
 def get_piece(player, king):
     if player == 1:
@@ -28,24 +28,70 @@ def pretty_print_board(board):
     print('[ ]' + f'[{pretty_board[24]}]' + '[ ]' + f'[{pretty_board[25]}]' + '[ ]' + f'[{pretty_board[26]}]' + '[ ]' + f'[{pretty_board[27]}]')
     print(f'[{pretty_board[28]}]' + '[ ]' + f'[{pretty_board[29]}]' + '[ ]' + f'[{pretty_board[30]}]' + '[ ]' + f'[{pretty_board[31]}]' + '[ ]')
 
+def print_board_coords():
+    print('[  ]' + '[ 1]' + '[  ]' + '[ 2]' + '[  ]' + '[ 3]' + '[  ]' + '[ 4]')
+    print('[ 5]' + '[  ]' + '[ 6]' + '[  ]' + '[ 7]' + '[  ]' + '[ 8]' + '[  ]')
+    print('[  ]' + '[ 9]' + '[  ]' + '[10]' + '[  ]' + '[11]' + '[  ]' + '[12]')
+    print('[13]' + '[  ]' + '[14]' + '[  ]' + '[15]' + '[  ]' + '[16]' + '[  ]')
+    print('[  ]' + '[17]' + '[  ]' + '[18]' + '[  ]' + '[19]' + '[  ]' + '[20]')
+    print('[21]' + '[  ]' + '[22]' + '[  ]' + '[23]' + '[  ]' + '[24]' + '[  ]')
+    print('[  ]' + '[25]' + '[  ]' + '[26]' + '[  ]' + '[27]' + '[  ]' + '[28]')
+    print('[29]' + '[  ]' + '[30]' + '[  ]' + '[31]' + '[  ]' + '[32]' + '[  ]')
+
+def get_heuristic_value(board):
+    black_pieces = 0
+    white_pieces = 0
+    for piece in board.pieces:
+        if piece.player == 1 and not piece.captured:
+            black_pieces += 1
+        elif piece.player == 2 and not piece.captured:
+            white_pieces += 1
+    if board.player_turn == 1:
+        return black_pieces / (black_pieces + white_pieces)
+    else:
+        return white_pieces / (black_pieces + white_pieces)
+
 def alphabeta(state, alpha = -INFINITY, beta = INFINITY, depth = 0):
     # need to deep copy the state, or be able to undo moves
-    print("Board:")
-    pretty_print_board(state.board)
-    old_board = state.board
+    #print("Board:")
+    #pretty_print_board(state.board)
     if state.is_over():
         if state.whose_turn == state.get_winner():
             return 1
         else:
             return -1
     if depth >= MAX_DEPTH:
-        return 0 # return draw
+        return get_heuristic_value(state.board)
     for m in state.get_possible_moves():
+        old_board = state.board
         state.move(m)
         value = -alphabeta(state, -beta, -alpha, depth + 1)
+        state.board = old_board
+
         if value > alpha:
             alpha = value
-        state.board = old_board
         if value >= beta:
             return beta
     return alpha
+
+def play_move(game):
+    moves = game.get_possible_moves()
+    ab_results = []
+    for i in range(len(moves)):
+        print(moves)
+        print(ab_results)
+        old_board = game.board
+        game.move(moves[i])
+        ab = alphabeta(game)
+        if ab == 1:
+            print(f"Found win: {moves[i]}")
+            return # This is the best we can do, so keep this move
+        ab_results.append(ab)
+        game.board = old_board
+    print(moves)
+    print(ab_results)
+
+    # Pick the move with the best score:
+    move_index = ab_results.index(max(ab_results))
+    print(f"Playing: {moves[move_index]}")
+    game.move(moves[move_index])
--- main.py
+++ main.py
@@ -5,4 +5,26 @@ import alphabeta
 
 game = Game()
 
-print(alphabeta.alphabeta(game))
+#print(alphabeta.alphabeta(game))
+
+while not game.is_over():
+    print("Board coords:")
+    alphabeta.print_board_coords()
+    print("Board:")
+    alphabeta.pretty_print_board(game.board)
+    print(game.get_possible_moves())
+    start_piece = int(input("Black piece to move: "))
+    end_pos = int(input("Where to move to: "))
+    game.move([start_piece, end_pos])
+
+    # Now white's turn:
+    alphabeta.play_move(game)
+
+print("Board coords:")
+alphabeta.print_board_coords()
+print("Board:")
+alphabeta.pretty_print_board(game.board)
+print(game.get_possible_moves())
+print(game.whose_turn())
+print(game.is_over())
+print(game.get_winner())
